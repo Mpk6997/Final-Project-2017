3a726f6a6d5a1108ac08f45c176f10080b75430a
Chaitanya Gangavarapu <chaitanya.gangavarapu@gmail.com>
2017-05-31 04:09:33 -0700

    added hover and sorted plot

--- scripts/plot.R
+++ scripts/plot.R
@@ -6,7 +6,11 @@ accountChart <- function(y.axis) {
   y <- list(
     title = y.axis
   )
-  plot_ly(data = df, x = ~followers, y = ~eval(parse(text = y.axis)),
+  df <- mutate_each(df, funs(gsub(",", "", .)), tweets, following, followers)
+  df <- mutate(df, tweets = as.numeric(as.character(df$tweets)), following = as.numeric(df$following),
+               followers = as.numeric(df$followers)) 
+  df <- arrange(df, tweets) %>% arrange(followers) %>% arrange(following)
+  plot_ly(data = df, x = ~followers, y = ~eval(parse(text = y.axis)), type = 'scatter', 
           # Hover Text
           text = ~username) %>% 
     layout(title = 'Top Accounts Twitter Data', yaxis = y)
